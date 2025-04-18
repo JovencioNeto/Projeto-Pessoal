602886eb139b2895d56e522606e8c9b4137f0969
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2025-04-18 19:22:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const readline = require('readline')
+
 const metrosForCM = require('./src/metrosForCM')
 const metrosForDAM = require('./src/metrosForDAM')
 const metrosForDM = require('./src/metrosForDM')
@@ -5,14 +7,60 @@ const metrosForKM = require('./src/metrosForKM')
 const metrosForMM = require('./src/metrosForMM')
 const metrosForHM = require('./src/metrosForHM')
 
-console.log(metrosForCM(200))
-console.log(metrosForDM(200))
-console.log(metrosForDAM(890))
-console.log(metrosForKM(30))
-console.log(metrosForMM(57))
-console.log(metrosForHM(1000))
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
+rl.question('Digite o valor em metros: ', (valor) => {
+  const metros = Number(valor)
 
-console.log(metrosForCM('beta'))
-console.log(metrosForHM('samuel'))
-console.log(metrosForKM('saruel'))
+  if (isNaN(metros)) {
+    console.log('Erro: o valor digitado não é um número.')
+    rl.close()
+    return
+  }
+
+  console.log('\nEscolha a unidade para conversão:')
+  console.log('1 - Centímetros (cm)')
+  console.log('2 - Decímetros (dm)')
+  console.log('3 - Decâmetros (dam)')
+  console.log('4 - Quilômetros (km)')
+  console.log('5 - Milímetros (mm)')
+  console.log('6 - Hectômetros (hm)\n')
+
+  rl.question('Digite o número da opção: ', (opcao) => {
+    let resultado
+
+    switch (opcao) {
+      case '1':
+        resultado = metrosForCM(metros)
+        console.log(`${metros} metros = ${resultado} centímetros`)
+        break
+      case '2':
+        resultado = metrosForDM(metros)
+        console.log(`${metros} metros = ${resultado} decímetros`)
+        break
+      case '3':
+        resultado = metrosForDAM(metros)
+        console.log(`${metros} metros = ${resultado} decâmetros`)
+        break
+      case '4':
+        resultado = metrosForKM(metros)
+        console.log(`${metros} metros = ${resultado} quilômetros`)
+        break
+      case '5':
+        resultado = metrosForMM(metros)
+        console.log(`${metros} metros = ${resultado} milímetros`)
+        break
+      case '6':
+        resultado = metrosForHM(metros)
+        console.log(`${metros} metros = ${resultado} hectômetros`)
+        break
+      default:
+        console.log('Opção inválida.')
+    }
+
+    rl.close()
+  })
+})
